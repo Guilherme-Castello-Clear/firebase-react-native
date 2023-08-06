1a6b49c63c992319b247051df8afa9a1a9ae4cd0
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-08-06 17:57:41 -0300

    Feat: TaskApp started

--- App.js
+++ App.js
@@ -1,69 +1,28 @@
 import React, { useState, useEffect} from 'react';
-import { View, Text, StyleSheet, Button, TextInput, FlatList } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, TextInput } from 'react-native';
 import firebase from './src/firebaseConnection';
+import Login from './src/Components/Login';
 
 export default function App(){
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState('')
 
-  async function logar(){
-    await firebase.auth().signInWithEmailAndPassword(email, password).then(value =>{
-      alert('Seja bem vindo, '+ value.user.email)
-      setUser(value.user.email)
-    }).catch(error => {
-      alert('Algo deu errado')
-    })
-  }
+  const [user, setUser] = useState(null)
 
-  async function logout(){
-    await firebase.auth().signOut();
-    setUser('')
-    alert('Deslogado com sucesso')
+  if(!user){
+    return <Login/>
   }
 
   return(
-    <View style={styles.container}>
-      <Text style={styles.texto}>Nome</Text>
-      <TextInput
-        style={styles.input}
-        underlineColorAndroid="transparent"
-        onChangeText={(email) => setEmail(email)}
-      />
-
-      <Text style={styles.texto}>Cargo</Text>
-      <TextInput
-        style={styles.input}
-        underlineColorAndroid="transparent"
-        onChangeText={(password) => setPassword(password)}
-      />
-
-      <Button
-        title="Logar"
-        onPress={logar}
-      />
-
-      <FlatList
-        keyExtractor={item => item.key}
-        data={usuarios}
-        renderItem={item => (<Listagem data={item}/>)}
-      />
-
-      <Text>
-        {user}
-      </Text>
-      {user.length > 0 && <Button
-        title="Logout"
-        onPress={logout}
-      />}
-
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Dentro de Tarefas</Text>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    margin: 10,
+    paddingTop: 25,
+    paddingHorizontal: 10,
+    backgroundColor: '#F2f6fc'
   }
 })
---
+++ src/Components/Login/index.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect} from 'react';
+import { View, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
+
+export default function Login(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [type, setType] = useState('login')
+  
+    function handleLogin(){
+        
+    }
+  
+    return(
+        <SafeAreaView style={styles.container}>
+            <TextInput
+                placeholder='Seu email'
+                style={styles.input}
+                value={email}
+                onChangeText={ text => setEmail(text)}
+            />
+
+            <TextInput
+                placeholder='*******'
+                style={styles.input}
+                value={password}
+                onChangeText={ text => setPassword(text)}
+            />
+
+            <TouchableOpacity
+                style={[styles.handleLogin, { backgroundColor: type === 'login' ? 'blue' : '#141414'}]}
+                onPress={handleLogin}
+            >
+                <Text>{type === 'login' ? 'Acessar' : 'Cadastrar'}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                onPress={ () => setType(type => type === 'login' ? 'cadastrar' : 'login')}
+            >
+                <Text style={{textAlign: 'center'}}>{type === 'login' ? 'Criar uma conta' : 'Já possuo uma conta'}</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+
+  );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        paddingTop: 40,
+        backgroundColor: '#F2f6fc',
+        paddingHorizontal: 10,
+      },
+      input:{
+        marginBottom: 10,
+        backgroundColor: '#FFF',
+        borderRadius: 4,
+        height: 45,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: '#141414'
+      },
+      handleLogin:{
+        alignItems: 'center',
+        justifyContent:'center',
+        height: 45,
+        marginBottom: 10,
+      },
+      loginText:{
+        color: '#FFF',
+        fontSize: 17,
+      }
+})
